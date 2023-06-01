ffbc986b28e81f09b055b2852263202d3aeb6f8f
marcelodoqa <hideo.toma@icloud.com>
2023-06-01 01:05:04 -0300

    function tests finish

--- README.md
+++ README.md
@@ -5,15 +5,16 @@ Repositório dedicado a fazer testes funcionais (e2e) utilizando o Cypress.
 
 1. O site escolhido a ser testado foi o ' [Seu Barriga](https://barrigareact.wcaquino.me/login) '  criado pelo professor Wagner Aquino a fins didáticos.
 
-2. [Cenario e casos de teste](https://docs.google.com/spreadsheets/d/1LnYMWvuqOVjX44v0KjdlTlnldO5o7Kur/edit?usp=sharing&ouid=104686743245628511880&rtpof=true&sd=true) documentado.
+2. [Casos de teste](https://docs.google.com/spreadsheets/d/1LnYMWvuqOVjX44v0KjdlTlnldO5o7Kur/edit?usp=sharing&ouid=104686743245628511880&rtpof=true&sd=true) documentado.
 
 3. [ Evidência de teste](https://docs.google.com/document/d/1WKwj7pamYH3jnRUpH949W1j4HtR5G6bXc4FZLZpVJOE/edit?usp=sharing) documentado. 
 
 4. Serão criados 3 arquivos de testes para mostrar maneiras diferentes de executar testes e2e:
    
-    - `functional-test.cy.js`
-    - `tests-with-commands.cy.js`
-    - `test-with-locator.cy.js`
+    - [`functional-test.cy.js`](cypress/e2e/functional-tests.cy.js)
+
+    - [`tests-with-commands.cy.js`](cypress/e2e/tests-with-commands.cy.js)
+    - [`test-with-locator.cy.js`](cypress/e2e/tests-with-locator.cy.js)
 </div>
 <br>
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', ()=>{
+        cy.get('[data-test="email"]').type('marcelo@mail.com')
+        cy.get('[data-test="passwd"]').type('senha123', {logFalse: true})
+        cy.get('.btn').click()
+        cy.get('.toast-message')
+            .should('be.visible')
+            .and('contain', 'Bem vindo,')
+})
+
+Cypress.Commands.add('resetar', ()=>{
+    cy.get('[data-test="menu-settings"]').click()
+    cy.get('[href="/reset"]').click()
+    cy.get('.toast-message')
+            .should('be.visible').and('contain', 'Dados resetados com sucesso!')
+})
